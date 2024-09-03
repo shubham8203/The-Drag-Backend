e50322e58f9a6d6b3cce35069eb257f71a86736b
shubham-d2itechnology <shubham.d2itechnology@gmail.com>
2024-09-03 13:38:56 +0530

    ""

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:'*',
+    origin:'http://localhost:3000',
     credentials:true,
-    
+        
+
 }));
 app.use(cookieParser());
 
