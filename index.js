797ba1a94f410058a5405f5d584e55fe52f7a20f
shubham8203 <shubhamchauhan8203@gmail.com>
2024-09-07 13:25:05 +0530

    ""

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:'https://drag-umber.vercel.app/',
+    origin:'*',
     credentials:true,
         
 
