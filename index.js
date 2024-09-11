b631ecacad3ed8c8fc4a128c60609ef986d6fa94
shubham8203 <116935952+shubham8203@users.noreply.github.com>
2024-09-11 13:27:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://the-drag-frontend-ruddy.vercel.app/',
     credentials:true,
         
 
